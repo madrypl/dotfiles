f60ff3ea6799f9637e8ed5415dde9a97fcae3b64
Artur Mądrzak <artur@madrzak.eu>
2021-07-07 14:48:25 +0200

    use .vim dir for configuration and install by symlinking

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ APT_PACKAGES=" \
 	vim \
 "
 
-
 install_apt()
 {
 	sudo /bin/sh -c "apt update && apt install --yes ${APT_PACKAGES}"
@@ -16,8 +15,7 @@ install_apt()
 
 install_vim()
 {
-	install --backup --mode 0644 -T "${PWD}/vimrc" "${HOME}/.vimrc"
-	install --backup --mode 0644 -T "${PWD}/vimrc-map" "${HOME}/.vimrc-map"
+	ln -s "${PWD}/vim" "${HOME}/.vim"
 }
 
 install_tmux()
