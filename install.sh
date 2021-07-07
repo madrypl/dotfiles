003423634085eff2be4fb1410128b7d8989eff3d
Artur Mądrzak <artur@madrzak.eu>
2021-07-07 16:09:29 +0200

    bugfixes for install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ APT_PACKAGES=" \
 	shellcheck \
 	tmux \
 	vim \
+    vim-gtk3 \
 "
 
 install_apt()
@@ -15,19 +16,35 @@ install_apt()
 
 install_vim()
 {
-	ln -s "${PWD}/vim" "${HOME}/.vim"
+    _vimdir="${HOME}/.vim"
+    if [ -e "${_vimdir}" ]; then
+        echo "Warning: Backuping '${_vimdir}' to '${_vimdir}.bak'"
+        mv -T "${_vimdir}" "${_vimdir}.bak"
+    fi
+	ln -sT "${PWD}/vim" "${_vimdir}"
 }
 
 install_tmux()
 {
-	install --backup --mode 0644 -T "${PWD}/tmux.conf" "${HOME}/.tmux.conf"
+    _tmuxconf="${HOME}/.tmux.conf"
+    if [ -e "${_tmuxconf}" ]; then
+        echo "Warning: Backuping '${_tmuxconf}' to '${_tmuxconf}.bak'"
+        mv "${_tmuxconf}" "${_tmuxconf}.bak"
+    fi
+    ln -sT "${PWD}/tmux.conf" "${_tmuxconf}"
+}
+
+install_fuzzy_completer()
+{
+    cd fzf && ./install --all
 }
 
 main()
 {
-#	install_apt
+	install_apt
 	install_vim
 	install_tmux
+    install_fuzzy_completer
 }
 
 main "${@}"
