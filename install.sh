c06456211e8c86a02061e2c1bd4d654d65a2e953
Artur Mądrzak <artur@madrzak.eu>
2021-07-08 15:15:46 +0200

    configure editorconfig, reindent install.sh

--- install.sh
+++ install.sh
@@ -4,32 +4,39 @@ set -eu
 
 APT_PACKAGES=" \
     editorconfig \
-	shellcheck \
-	tmux \
-	vim \
+    shellcheck \
+    tmux \
+    vim \
     vim-gtk3 \
-"
+    "
 
 install_apt()
 {
-	sudo /bin/sh -c "apt update && apt install --yes ${APT_PACKAGES}"
+    for _pkg in ${APT_PACKAGES}; do
+        if ! dpkg -s "${_pkg}" >/dev/null 2>&1; then
+            _not_installed="${_not_installed:-} ${_pkg}"
+        fi
+    done
+    if [ -n "${_not_installed:-}" ]; then
+        sudo /bin/sh -c "apt update && apt install --yes ${_not_installed}"
+    fi
 }
 
 install_vim()
 {
     _vimdir="${HOME}/.vim"
     if [ -e "${_vimdir}" ]; then
-        echo "Warning: Backuping '${_vimdir}' to '${_vimdir}.bak'"
+        echo "Warning: Backing up '${_vimdir}' to '${_vimdir}.bak'"
         mv -T "${_vimdir}" "${_vimdir}.bak"
     fi
-	ln -sT "${PWD}/vim" "${_vimdir}"
+    ln -sT "${PWD}/vim" "${_vimdir}"
 }
 
 install_tmux()
 {
     _tmuxconf="${HOME}/.tmux.conf"
     if [ -e "${_tmuxconf}" ]; then
-        echo "Warning: Backuping '${_tmuxconf}' to '${_tmuxconf}.bak'"
+        echo "Warning: Backing up '${_tmuxconf}' to '${_tmuxconf}.bak'"
         mv "${_tmuxconf}" "${_tmuxconf}.bak"
     fi
     ln -sT "${PWD}/tmux.conf" "${_tmuxconf}"
@@ -37,15 +44,26 @@ install_tmux()
 
 install_fuzzy_completer()
 {
-    cd fzf && ./install --all
+    ./fzf/install --all > /dev/null
+}
+
+install_editorconfig()
+{
+    _target="${HOME}/.editorconfig"
+    if [ -e "${_target}" ]; then
+        echo "Warning: Backing up '${_target}' to '${_target}.bak'"
+        mv "${_target}" "${_target}.bak"
+    fi
+    ln -sT "${PWD}/editorconfig" "${_target}"
 }
 
 main()
 {
-	install_apt
-	install_vim
-	install_tmux
+    install_apt
+    install_vim
+    install_tmux
     install_fuzzy_completer
+    install_editorconfig
 }
 
 main "${@}"
